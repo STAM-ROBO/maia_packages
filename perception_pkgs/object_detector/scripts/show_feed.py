a45c6ae842bf924293f83d611cacccb9a1632330
administrator <d.fusco@stamtech.com>
2024-04-18 11:50:57 +0200

    TensorRT added

--- perception_pkgs/object_detector/scripts/show_feed.py
+++ perception_pkgs/object_detector/scripts/show_feed.py
@@ -15,6 +15,7 @@ from torchvision.transforms.functional import to_pil_image
 
 from mobile_net_backend.mobilenet_detector import MBNet_Detector
 from yolov7_backend.yolov7_detector import YOLO7_Detector
+from yolov7_backend.yolo7_trt import YOLO7_TRT
 import message_filters
 from cv_bridge import CvBridge, CvBridgeError
 from object_detector.msg import Detections_3d,Object_3d
@@ -23,14 +24,14 @@ class Detector:
     def __init__(self):
         import sys
         print(sys.executable)
-        self.detector = YOLO7_Detector()
+        self.detector = YOLO7_TRT()
         image_sub = message_filters.Subscriber('/camera/color/image_raw', Image)
         depth_sub = message_filters.Subscriber('/camera/aligned_depth_to_color/image_raw', Image)
         info = message_filters.Subscriber('/camera/color/camera_info', CameraInfo)
 
-        ts = message_filters.TimeSynchronizer([image_sub,depth_sub,info], 5)
+        ts = message_filters.TimeSynchronizer([image_sub,depth_sub,info],100)
         ts.registerCallback(self.callback)
-        self.pub = rospy.Publisher('detections_3d', Detections_3d, queue_size=10)
+        self.pub = rospy.Publisher('detections_3d', Detections_3d, queue_size=180)
         self.names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
          'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
          'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
@@ -47,33 +48,29 @@ class Detector:
         realsense_depth_scale = 0.001
         depth_img = np.frombuffer(depth.data, dtype=np.uint16).reshape((720,1280))*realsense_depth_scale
         
-        
-        
-        #bridge = CvBridge()
-        
-        #print(depth_image.shape)
-        #np.save('depth_bin.npy',depth)
         k = np.array(list(info.K)).reshape(3,3)
         inv_k = np.linalg.pinv(k)
         
-        #print(k)
         image_numpy = image_numpy.reshape((720,1280,3))
         image_numpy = cv2.cvtColor(image_numpy,cv2.COLOR_RGB2BGR)
+        
         boxes,labels,scores = self.detector.detect(image_numpy)
-        print(labels,scores)
+        
         msg = Detections_3d()
         msg.header =  std_msgs.msg.Header()
         msg.header.stamp = rospy.Time.now()
         objs = []
         for box,label in zip(boxes,labels):
-            print(label)
+            
             xmin,ymin,xmax,ymax =[int(b) for b in box]
             cv2.rectangle(image_numpy,(xmin,ymin),(xmax,ymax),[255,255,0],2)
             center_x = int((xmin+xmax)/2)
             center_y = int((ymin+ymax)/2)
             z = depth_img[center_y,center_x]
+            if(z<0.01):
+                continue
             point2d_z = np.array([center_x*z,center_y*z,z])
-            point_3d = np.dot(inv_k,point2d_z)
+            point_3d= np.dot(inv_k,point2d_z)
             
             cv2.putText(image_numpy, '%.2fm'%(point_3d[2]),(xmin+20,ymin+20),cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
             obj = Object_3d()
--- perception_pkgs/object_detector/src/yolov7_backend/yolo7_trt.py
+++ perception_pkgs/object_detector/src/yolov7_backend/yolo7_trt.py
@@ -18,105 +18,24 @@ import tensorrt as trt
 from PIL import Image
 from pathlib import Path
 from collections import OrderedDict,namedtuple
-class_id_to_string = {0: u'__background__',
- 1: u'person',
- 2: u'bicycle',
- 3: u'car',
- 4: u'motorcycle',
- 5: u'airplane',
- 6: u'bus',
- 7: u'train',
- 8: u'truck',
- 9: u'boat',
- 10: u'traffic light',
- 11: u'fire hydrant',
- 12: u'stop sign',
- 13: u'parking meter',
- 14: u'bench',
- 15: u'bird',
- 16: u'cat',
- 17: u'dog',
- 18: u'horse',
- 19: u'sheep',
- 20: u'cow',
- 21: u'elephant',
- 22: u'bear',
- 23: u'zebra',
- 24: u'giraffe',
- 25: u'backpack',
- 26: u'umbrella',
- 27: u'handbag',
- 28: u'tie',
- 29: u'suitcase',
- 30: u'frisbee',
- 31: u'skis',
- 32: u'snowboard',
- 33: u'sports ball',
- 34: u'kite',
- 35: u'baseball bat',
- 36: u'baseball glove',
- 37: u'skateboard',
- 38: u'surfboard',
- 39: u'tennis racket',
- 40: u'bottle',
- 41: u'wine glass',
- 42: u'cup',
- 43: u'fork',
- 44: u'knife',
- 45: u'spoon',
- 46: u'bowl',
- 47: u'banana',
- 48: u'apple',
- 49: u'sandwich',
- 50: u'orange',
- 51: u'broccoli',
- 52: u'carrot',
- 53: u'hot dog',
- 54: u'pizza',
- 55: u'donut',
- 56: u'cake',
- 57: u'chair',
- 58: u'couch',
- 59: u'potted plant',
- 60: u'bed',
- 61: u'dining table',
- 62: u'toilet',
- 63: u'tv',
- 64: u'laptop',
- 65: u'mouse',
- 66: u'remote',
- 67: u'keyboard',
- 68: u'cell phone',
- 69: u'microwave',
- 70: u'oven',
- 71: u'toaster',
- 72: u'sink',
- 73: u'refrigerator',
- 74: u'book',
- 75: u'clock',
- 76: u'vase',
- 77: u'scissors',
- 78: u'teddy bear',
- 79: u'hair drier',
- 80: u'toothbrush'}
 class YOLO7_TRT:
     def __init__(self):
         print(torch.cuda.is_available())
         self.device = torch.device('cuda')
-        model_weights = './yolov7-nms.trt'
+        model_weights = '/home/administrator/ITTSorting/src/object_detector/src/yolov7_backend/yolov7-nms.trt'
         Binding = namedtuple('Binding', ('name', 'dtype', 'shape', 'data', 'ptr'))
         logger = trt.Logger(trt.Logger.ERROR)
         trt.init_libnvinfer_plugins(logger, namespace="")
-        with open(w, 'rb') as f, trt.Runtime(logger) as runtime:
-        model = runtime.deserialize_cuda_engine(f.read())
+        with open(model_weights, 'rb') as f, trt.Runtime(logger) as runtime:
+            model = runtime.deserialize_cuda_engine(f.read())
         self.bindings = OrderedDict()
         for index in range(model.num_bindings):
             name = model.get_tensor_name(index)
             dtype = trt.nptype(model.get_tensor_dtype(name))
             shape = tuple(model.get_tensor_shape(name))
-            data = torch.from_numpy(np.empty(shape, dtype=np.dtype(dtype))).to(device)
-            bindings[name] = Binding(name, dtype, shape, data, int(data.data_ptr()))
-        binding_addrs = OrderedDict((n, d.ptr) for n, d in bindings.items())
+            data = torch.from_numpy(np.empty(shape, dtype=np.dtype(dtype))).to(self.device)
+            self.bindings[name] = Binding(name, dtype, shape, data, int(data.data_ptr()))
+        self.binding_addrs = OrderedDict((n, d.ptr) for n, d in self.bindings.items())
         self.context = model.create_execution_context()
     def letterbox(self,im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleup=True, stride=32):
         # Resize and pad image while meeting stride-multiple constraints
@@ -145,27 +64,38 @@ class YOLO7_TRT:
         left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
         im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
         return im, r, (dw, dh)
+    def postprocess(self,boxes,r,dwdh):
+        dwdh = torch.tensor(dwdh*2).to(boxes.device)
+        boxes -= dwdh
+        boxes /= r
+        return boxes
+
     def detect(self,image_numpy):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(image_numpy, cv2.COLOR_BGR2RGB)
         image = img.copy()
-        image, ratio, dwdh = letterbox(image, auto=False)
+        image, ratio, dwdh = self.letterbox(image, auto=False)
         image = image.transpose((2, 0, 1))
         image = np.expand_dims(image, 0)
         image = np.ascontiguousarray(image)
 
         im = image.astype(np.float32)
-        im = torch.from_numpy(im).to(device)
+        im = torch.from_numpy(im).to(self.device)
         im/=255     
 
-        binding_addrs['images'] = int(im.data_ptr())
-        context.execute_v2(list(binding_addrs.values()))
-        nums = bindings['num_dets'].data
-        boxes = bindings['det_boxes'].data
-        scores = bindings['det_scores'].data
-        classes = bindings['det_classes'].data
+        self.binding_addrs['images'] = int(im.data_ptr())
+        import time
+        start = time.time()
+
+        self.context.execute_v2(list(self.binding_addrs.values()))
+        #print('cost is %.5f '%(time.time()-start))
+        nums = self.bindings['num_dets'].data
+        boxes = self.bindings['det_boxes'].data
+        scores = self.bindings['det_scores'].data
+        classes = self.bindings['det_classes'].data
   
         boxes = boxes[0,:nums[0][0]]
-        scores = scores[0,:nums[0][0]]
-        classes = classes[0,:nums[0][0]]
-        boxs = [postprocess(b,ratio,dwdh).round().int() for b in boxes]
+        scores = scores[0,:nums[0][0]].detach().cpu().numpy()
+        classes = classes[0,:nums[0][0]].detach().cpu().numpy()
+        boxs = [self.postprocess(b,ratio,dwdh).round().int() for b in boxes]
+        boxes = [b.detach().cpu().numpy() for b in boxes]
         return boxes,classes,scores
