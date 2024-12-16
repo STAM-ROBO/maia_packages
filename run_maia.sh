774a9b6f274dc859f6fb34d3946a2f980a4a99f3
Davide Di Gloria <d.digloria@stamtech.com>
2024-12-16 16:03:43 +0100

    setup after reinstall

--- run_maia.sh
+++ run_maia.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #trigger udev rule to assign symbolic device names to physical USB devices
 #rules defined by Davide Di Gloria into /etc/udev/rules.d/99-usb-serial.rules 
-sudo udevadm trigger
+#sudo udevadm trigger
 
 #get permissions for ttys
-sudo chmod a+rw /dev/usb_lidar
-sudo chmod a+rw /dev/usb_motors
-#sudo chmod a+rw /dev/ttyUSB0
-#sudo chmod a+rw /dev/ttyUSB1
+#sudo chmod a+rw /dev/usb_lidar
+#sudo chmod a+rw /dev/usb_motors
+sudo chmod a+rw /dev/ttyUSB0
+sudo chmod a+rw /dev/ttyACM0
 
 
 #again, set the IP address of roscore
-export ROS_IP=172.17.24.54
+export ROS_IP=172.17.24.63
 
 #set IP address of roscore
-export ROS_MASTER_URI=http://172.17.24.54:11311
+export ROS_MASTER_URI=http://172.17.24.63:11311
 
 cd 
 cd maia_ws
